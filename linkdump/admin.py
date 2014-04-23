ebaae5e76f999db437a299a20e9222291d2ff884
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-04-23 10:56:41 -0400

    Condense the DumpAdmin Form
    * Modify the DumpAdmin to only show the title, link, description and
      category fields.
    * Modify the DumpAdmin to automatically use the current User for the
      user field.

--- linkdump/admin.py
+++ linkdump/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from mezzanine.core.admin import OwnableAdmin
 from mptt.admin import MPTTModelAdmin
 
 from .models import Dump, DumpCategory
@@ -11,7 +12,17 @@ class DumpAdmin(admin.ModelAdmin):
                     "user")
     list_filter = ("user", "category")
     search_fields = ("title", "description", "category__title")
+    fields = ("title", "link", "description", "category")
     date_hierarchy = "created"
+
+    def save_form(self, request, form, change):
+        """
+        Set the object's owner as the logged in user.
+        """
+        obj = form.save(commit=False)
+        if obj.user_id is None:
+            obj.user = request.user
+        return super(DumpAdmin, self).save_form(request, form, change)
 
 
 class DumpCategoryAdmin(MPTTModelAdmin):
